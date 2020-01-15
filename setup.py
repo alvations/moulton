eaba8f718eb3b5486c66a5fb4e78e1cbde4cd92f
alvations <alvations@gmail.com>
2020-01-15 10:52:28 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+
+
+setup(
+    name='moulton',
+    description='',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+        "pyderman",
+        "beautifulsoup4",
+        "tqdm",
+        "pprint",
+        "requests"
+    ],
+    }
+)
